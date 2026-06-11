239f2777ff4c74aa1cb71b48496e1b8a61c653e9
Brun0Silveir4 <bruno.silveira.dionisio@gmail.com>
2026-06-11 11:11:15 -0300

    add: criando swagger de forma automática

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,21 @@ async function bootstrap() {
       transform: true
     })
   )
+
+  const config = new DocumentBuilder()
+  .setTitle('CRUD Items API')
+  .setDescription('API para o meu CRUD de items')
+  .setVersion('1.0')
+  .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+  
+SwaggerModule.setup('swagger', app, document, {
+  jsonDocumentUrl: 'swagger/json',
+});
+
+
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
